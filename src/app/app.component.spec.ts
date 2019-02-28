81d4d827d4673bd3a21f33cb01ba85f362efde7e
JaviGuz <javier.guzman@feverup.com>
2019-02-28 13:10:20 +0100

    jest configuration

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,22 +10,22 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  test('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'heroesApp'`, () => {
+  test(`the title is 'heroesApp'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('heroesApp');
-  });
+  }));
 
-  it('should render title in a h1 tag', () => {
+  test('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to heroesApp!');
-  });
+  }));
 });
